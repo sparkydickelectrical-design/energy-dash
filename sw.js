10f03faed42e6c196fdfff2e6f844b02f33b1cea
sparkydickelectrical-design <sparkydickelectrical@gmail.com>
2026-02-21 16:17:07 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'energy-hub-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/manifest.json',
+  '/icon.png'
+];
+
+// Install the service worker and cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
