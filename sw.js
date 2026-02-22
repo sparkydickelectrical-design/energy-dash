10ed02437537fae0764ad2973d06293b0330fa3b
sparkydickelectrical-design <sparkydickelectrical@gmail.com>
2026-02-22 12:05:21 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'energy-hub-v48';
+const ASSETS = ['index.html', 'style.css', 'manifest.json', 'app-icon.png', 'savings.html'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
